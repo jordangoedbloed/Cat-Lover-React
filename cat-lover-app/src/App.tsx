b26b5d7d57ceb78a79dd4a2736cb5618aaec8e28
Jordan Goedbloed <jr.goedbloed@gmail.com>
2024-01-25 18:20:28 +0100

    outcomment a line

--- cat-lover-app/src/App.tsx
+++ cat-lover-app/src/App.tsx
@@ -1,28 +1,3 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Home from "./components/Home";
-// import Kitties from "./components/Kitties";
-// import CreateKittyPage from "./components/CreateKittyPage";
-// // import KittyDetails from "./components/KittyDetails";
-// import "./App.css";
-
-// function App() {
-//   return (
-//     <>
-//       <BrowserRouter>
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/about" element={<h1>About</h1>} />
-//           <Route path="/kitties" element={<Kitties />} />
-//           <Route path="/kitties/create" element={<CreateKittyPage />} />
-//           <Route path="*" element={<h1>Not Found</h1>} />
-//         </Routes>
-//       </BrowserRouter>
-//     </>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Kitties from "./components/Kitties";
